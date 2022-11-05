77ef272f06503495913fbd8efb710945d580eb3a
giu-ferreira-cientista <giuferreira@gmail.com>
2022-11-05 16:01:28 +0000

    Infrastructure Ready

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# modern-data-stack-pipeline
-Repositorio para armazenar os artefatos do projeto de pipeline com modern data stack
+# airbyte-dbt-airflow-snowflake-metabase
+Repositório para armazenar os artefatos do Pipeline utilizando Modern Data Stack com AirByte + DBT + Airflow + SnowFlake + Metabase
+
+
+Tarefas:
+
+Infraestrutura:
+
+- Subir o Airbyte via docker X
+
+- Subir o Airflow via docker X
+
+- Subir o Metabase via docker X
+
+- Criar o script de execução X
+
+- Testar a Execução X
+
+- Criar a Conta no Dbt Cloud X
+
+- Criar a Conta no SnowFlake X
+
+- Snowflake Data Warehouse:
+    
+    - Verificando a existência das tabelas X
+    - Obtenção dos links de conexão e nome da conta X
+
+
+Extração:
+
+- No Airbyte:
+
+    - Conectar com as origens baseadas nos Csvs X
+    - Criar as entidades no snowflake através do script base da documentação    X
+    - Conectar o destino no snowflake X
+    - Criar as conexões do airbyte associando as origens ao destino X
+    - Testar as conexões X
+
+
+Preparação:
+
+- No Airbyte:
+
+    - Local Staging (Desenvolvimento)
+    - Cloud Staging (Produção)
+
+
+Transformação:
+
+- No Dbt:
+
+    - Criação da Conta
+    - Conexão com o Github
+    - Criação do Dbt Project
+    - Criação do Profile de conexão com o snowflake
+    - Criação do Schema
+    - Criação dos Modelos Base
+    - Criação do Modelo Relacionado
+    - Visualização gráfica do modelo
+    - Teste de execução
+    - Commits, Branches, Pull Requests, Merges no Github
+    - Obtenção do link de conexão com o Airbyte
+
+
+Visualização:
+
+- No Metabase:
+
+    - Conectar Metabase com o Snowflake
+    - Criar uma Question
+    - Criar um Dashboard
+    - Adicionar uma Question
+    - Visualizar o Resultado
+
+
+Orquestração:
+
+- No Airflow:
+
+    - Criar a dag
+
+    - Criar as conexões com o Airbyte através do script
+
+    - Testar a execução do pipeline
---
+++ setup.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+up() {
+  echo "Starting Airbyte..."
+  docker-compose -f docker-compose-airbyte.yaml down -v
+  docker-compose -f docker-compose-airbyte.yaml up -d
+
+  echo "Starting Airflow..."
+  docker-compose -f docker-compose-airflow.yaml down -v
+  mkdir -p ./dags ./logs ./plugins
+  echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" >> .env
+  docker-compose -f docker-compose-airflow.yaml up airflow-init
+  docker-compose -f docker-compose-airflow.yaml up -d
+    
+  echo "Starting Metabase..."
+  cd metabase
+  docker pull metabase/metabase:latest
+  docker run -d -p 3000:3000 --name metabase metabase/metabase
+  cd ..
+
+  echo "Access Metabase at http://localhost:3000 and set up a connection with Snowflake."
+
+  echo "Access Airbyte at http://localhost:8000 and set up the connections."
+  
+  echo "Access Airflow at http://localhost:8080 to kick off your Airbyte sync DAG."  
+}
+
+config() {
+  echo "Connecting Airflow with Airbyte..."
+  echo "Enter your Airbyte Epidemiology connection ID: "
+  read epidemiology_connection_id
+
+  echo "Enter your Airbyte Economy connection ID: "
+  read economy_connection_id
+
+  echo "Enter your Airbyte Demographics connection ID: "
+  read demographics_connection_id
+
+  echo "Enter your Airbyte Index connection ID: "
+  read index_connection_id
+
+  # Set connection IDs for DAG.
+  docker-compose -f docker-compose-airflow.yaml run airflow-webserver airflow variables set 'AIRBYTE_EPIDEMIOLOGY_CONNECTION_ID' "$epidemiology_connection_id"
+  docker-compose -f docker-compose-airflow.yaml run airflow-webserver airflow variables set 'AIRBYTE_ECONOMY_CONNECTION_ID' "$economy_connection_id"
+  docker-compose -f docker-compose-airflow.yaml run airflow-webserver airflow variables set 'AIRBYTE_DEMOGRAPHICS_CONNECTION_ID' "$demographics_connection_id"
+  docker-compose -f docker-compose-airflow.yaml run airflow-webserver airflow variables set 'AIRBYTE_INDEX_CONNECTION_ID' "$index_connection_id"
+
+  docker-compose -f docker-compose-airflow.yaml run airflow-webserver airflow connections add 'airbyte_example' --conn-uri 'airbyte://host.docker.internal:8000'
+
+}
+
+
+down() {
+  echo "Stopping Airbyte..."
+  docker-compose -f docker-compose-airbyte.yaml down -v
+  echo "Stopping Airflow..."
+  docker-compose -f docker-compose-airflow.yaml down -v
+  echo "Stopping Metabase..."
+  docker stop metabase
+}
+
+case $1 in
+  up)
+    up
+    ;;
+  down)
+    down
+    ;;
+  *)
+    echo "Usage: $0 {up|down}"
+    ;;
+esac
